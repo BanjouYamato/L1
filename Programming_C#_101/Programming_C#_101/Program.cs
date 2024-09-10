bc8a68ad39053ee39d2a53d289baa6c7d6ec7f93
BanjouYamato <lelong1503201@gmail.com>
2024-09-10 22:54:10 +0700

    nop bai

---
+++ Programming_C#_101/Programming_C#_101/Program.cs
@@ -0,0 +1,19 @@
+﻿//Part 1 Print a string to the console
+//hint: Console.WriteLine("Hello World!");
+Console.WriteLine("Hello World!");
+//Part 2 Get user input and print it to the console
+//hint: Console.ReadLine();
+Console.WriteLine("input: ");
+string user = Console.ReadLine();
+Console.WriteLine(user);
+//Part 3 Get two inputs from the user and print them on separate lines
+//hint: use \n to create a new line
+Console.WriteLine("input 1: ");
+string user1 = Console.ReadLine();
+Console.WriteLine("input 2: ");
+string user2 = Console.ReadLine();
+Console.WriteLine(user1 + "\n" + user2);
+
+Console.ReadKey();
+
+
